cc07c53172bec122637a219b2b0fcd0ce805eba2
ismetacar <acar.dogan@bil.omu.edu.tr>
2014-10-16 01:46:03 +0300

    tutti pabuc

--- ReplaceUnderscoreWithSpace.cs
+++ ReplaceUnderscoreWithSpace.cs
@@ -4,22 +4,24 @@ namespace UnderScoreToSpace
 {
 	class UnderScoreToSpace
 	{
+		/* ReplaceUnderscoreWithSpace fonksiyonu argüman olarak gelen stringi 
+		   önce karakter dizisine çeviriyor, ardından bu dizi elemanları üzerinde
+		   dolaşarak (dizinin ilk ve son elemanı hariç) '_' karakterini ' ' (space)
+		   olarak güncelliyor. */
 		static void ReplaceUnderscoreWithSpace(string a)
 		{
 			char[] array = a.ToCharArray();
-			for (int i = 1; i < array.Length-1; i++ )
-			{
-				if (array[i] == '_')
-				{
-					array[i] = ' ';
-				}
-			}
+			for (int i = 1; i < array.Length-1; i++)
+				if (array[i] == '_')  array[i] = ' ';
 
 			string s = new string(array);
 
 			System.Console.WriteLine(s);
 			
 		}
+		/* Main fonksiyonu bir argüman listesi alıyor. Bu argüman listesi String türünde. 
+		   Yine main fonksiyonu bu listenin ilk elemanından başlayarak ReplaceUnderscoreWithSpace
+		   fonksiyonuna tabii tutuyor. */
 		static void Main(String[] args)
 		{
 			for(int i = 0; i < args.Length; i++)
